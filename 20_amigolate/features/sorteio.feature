c8aa7d61de5ebbb090bbb3f20d95d1a28510b688
Marco Moura <marco.moura@gmail.com>
2010-12-22 08:17:37 -0200

    adicionar teste do amigolate

---
+++ 20_amigolate/features/sorteio.feature
@@ -0,0 +1,15 @@
+#language: pt
+
+Funcionalidade: Sorteio
+  Para poder comer chocolates
+  Como um gordo
+  Eu quero organizar um amigolate
+
+  Cenario: sortear o primeiro par
+    Dado que tenho "Natalia" e "Marcos" cadastrados
+    Quando eu mando sortear
+    Entao quero ver a dupla
+  
+  
+  
+  
---
+++ 20_amigolate/features/step_definitions/amigo_steps.rb
@@ -0,0 +1,13 @@
+
+Dado /^que tenho "([^"]*)" e "([^"]*)" cadastrados$/ do |arg1, arg2|
+	Amigolate.new
+end
+
+
+Quando /^eu mando sortear$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Entao /^quero ver a dupla$/ do
+  pending # express the regexp above with the code you wish you had
+end
